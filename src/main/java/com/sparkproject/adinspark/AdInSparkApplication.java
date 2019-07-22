ffa2e6dbdc853da7f6eed434621dc601868867c0
zhangyachao <zhangyachao@wps.cn>
2019-07-22 16:46:09 +0800

    init commit

---
+++ src/main/java/com/sparkproject/adinspark/AdInSparkApplication.java
@@ -0,0 +1,17 @@
+package com.sparkproject.adinspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author buildupchao
+ * @date 2019/7/22
+ */
+@SpringBootApplication
+public class AdInSparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdInSparkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparkproject/adinspark/AdInSparkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sparkproject.adinspark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdInSparkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
